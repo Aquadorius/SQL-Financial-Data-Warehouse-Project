d4b34b7171ea8b589a46379b13ac73dacca4881d
Muhammad Abdullah <136159195+Aquadorius@users.noreply.github.com>
2025-09-17 20:46:44 +0500

    Create bronze_load.sql

---
+++ bronze/bronze_load.sql
@@ -0,0 +1,31 @@
+/*=========================================
+INSERTING DATA INTO TABLE : bronze.fact_company
+  ===========================================
+*/
+PRINT('>>Truncating Table:  bronze.fact_company');
+TRUNCATE TABLE bronze.fact_company;
+GO
+PRINT('>>Inserting Data into: bronze.fact_company');
+GO
+BULK INSERT bronze.fact_company
+FROM 'D:\Project\AABS1.txt'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR='\t',
+TABLOCK);
+
+/*=========================================
+INSERTING DATA INTO TABLE : bronze.dim_company
+  ===========================================
+*/
+PRINT('>>Truncating Table:  bronze.dim_company');
+TRUNCATE TABLE bronze.fact_company;
+GO
+PRINT('>>Inserting Data into: bronze.dim_company');
+GO
+BULK INSERT bronze.dim_company
+FROM 'D:\Project\Company Codes.csv'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR=',',
+TABLOCK);
